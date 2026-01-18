1cba835e857bc4d6648c2735ba629b470b75e4e0
gon3again <dustinrose.dr@gmail.com>
2026-01-18 17:47:21 +0100

    added calculator as an example to work on

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from google.genai import types
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 # Now we can access `args.user_prompt`
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -19,16 +20,18 @@ if api_key == None:
 
 client = genai.Client(api_key=api_key)
 
-content_response = client.models.generate_content(model="gemini-2.5-flash",contents=args.user_prompt)
-if content_response.usage_metadata != None:
-    prompt_tokens = content_response.usage_metadata.prompt_token_count
-    response_tokens = content_response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
+if response.usage_metadata != None:
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 else:
     raise RuntimeError("usage_metadate = None")
 
-print(content_response.text)
+print(response.text)
 
 
 
