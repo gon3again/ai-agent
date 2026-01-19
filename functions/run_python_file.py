0c756d7b47f3fc6b16f02f204531367c3f6960bb
gon3again <dustinrose.dr@gmail.com>
2026-01-19 23:38:18 +0100

    run_python_file subprocess and output string

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -10,16 +10,29 @@ def run_python_file(working_directory, file_path, args=None):
         
         if not os.path.isfile(target_file):
             return f'Error: "{file_path}" does not exist or is not a regular file'
-        
+
         if not target_file.endswith('.py'):
-            f'Error: "{file_path}" is not a Python file'
+            return f'Error: "{file_path}" is not a Python file'
         
         command = ["python", target_file]
         if args!= None:
             command.extend(args)
         #continue here_________________________________________________________________________________________
-        #proc = subprocess.run(command,)
+        result = subprocess.run(command,capture_output=True,timeout=30,text=True)
+        return_code = result.returncode
+        output = ""
+        if return_code != 0:
+            output += f"Process exited with code {return_code}"
+        if result.stderr == "" and result.stdout =="":
+            output += "No output produced"
+        else:
+            output += f"STDOUT:{result.stdout}STDERR:{result.stderr}"
         
+        return output
+   
 
     except Exception as e:
-        return f'Error: {e}'
+        return f"Error: executing Python file: {e}"
+    
+
+#print(run_python_file(".","placeholder.py"))
